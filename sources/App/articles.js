1480d2085e641782b4f2eb95c41acc29ad599da7
abhixyz1 <abhinayazrf@gmail.com>
2025-07-22 18:32:06 +0700

    first

---
+++ sources/App/articles.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: "Cristiano Ronaldo",
+    image: "/assets/images1.jpg"
+  },
+  {
+    title: "Kylian Mbappé",
+    image: "/assets/images2.jpg"
+  },
+  {
+    title: "Christian Pulisic",
+    image: "/assets/images3.jpg"
+  },
+  {
+    title: "Jude Bellingham",
+    image: "/assets/images4.jpg"
+  },
+  {
+    title: "Neymar Jr",
+    image: "/assets/images5.jpg"
+  },
+  {
+    title: "Lionel Messi",
+    image: "/assets/images6.jpg"
+  },
+  {
+    title: "Bruno Fernandes",
+    image: "/assets/images7.jpg"
+  },
+  {
+    title: "Harry Kane",
+    image: "/assets/images8.jpg"
+  },
+  {
+    title: "Kevin De Bruyne",
+    image: "/assets/images9.jpg"
+  }
+];
